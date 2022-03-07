e350943f055aee70d5ececa3200186a7d5cf1587
Duc Nguyen <ducnguyen0305@gmail.com>
2022-03-06 20:52:00 -0800

    Create React app - frontend.

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>amazona</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
